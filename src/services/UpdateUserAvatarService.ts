eeba1b6f8e3cc449431d153754c3d7dd26124431
Victor Hugo do Nascimento <victor.hugo.nascimento58@gmail.com>
2020-08-27 22:56:19 -0300

    Classe de erro

--- src/services/UpdateUserAvatarService.ts
+++ src/services/UpdateUserAvatarService.ts
@@ -4,6 +4,7 @@ import { getRepository } from 'typeorm';
 
 import uploadConfig from '../config/upload';
 import User from '../models/User';
+import AppError from '../errors/AppError';
 
 interface Request {
   userId: string;
@@ -16,7 +17,8 @@ class UpdateUserAvatarService {
 
     const user = await usersRepository.findOne(userId);
 
-    if (!user) throw new Error('Only autheticated users can change avatar.');
+    if (!user)
+      throw new AppError('Only autheticated users can change avatar.', 401);
 
     if (user.avatar) {
       const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
