8f7432cc04d2124899767e6b8212d97b5439e4e5
Victor Hugo do Nascimento <victor.hugo.nascimento58@gmail.com>
2020-08-27 22:45:07 -0300

    Atualizando avatar

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,9 +2,12 @@ import path from 'path';
 import crypto from 'crypto';
 import multer from 'multer';
 
+const tempFolter = path.resolve(__dirname, '..', '..', 'temp');
+
 export default {
+  directory: tempFolter,
   storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'temp'),
+    destination: tempFolter,
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString('hex');
       const fileName = `${fileHash}-${file.originalname}`;
