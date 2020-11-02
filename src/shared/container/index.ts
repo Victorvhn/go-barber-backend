f6394e3d7caf0c51633cc1ec7f1b75fe3f7ab86b
Victor Hugo do Nascimento <victor.hugo.nascimento58@gmail.com>
2020-11-02 15:04:13 -0300

    Salvando Token no banco

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -9,8 +9,15 @@ import IAppointmentRepository from '@modules/appointments/repositories/IAppointm
 import IUserRepository from '@modules/users/repositories/IUserRepository';
 import UserRepository from '@modules/users/infra/typeorm/repositories/UserRepository';
 
+import IUserTokensRepository from '@modules/users/repositories/IUserTokenRepository';
+import UserTokenRepository from '@modules/users/infra/typeorm/repositories/UserTokenRepository';
+
 container.registerSingleton<IAppointmentRepository>(
   'AppointmentRepository',
   AppointmentRepository,
 );
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
+container.registerSingleton<IUserTokensRepository>(
+  'UserTokenRepository',
+  UserTokenRepository,
+);
