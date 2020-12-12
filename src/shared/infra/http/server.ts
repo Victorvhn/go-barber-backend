00fc1bdce1b084260e886990b6c55498430ee604
Victor Hugo do Nascimento <victor.hugo.nascimento58@gmail.com>
2020-12-12 16:04:46 -0300

    Validando dados

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import express from 'express';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import 'express-async-errors';
 
 import uploadConfig from '@config/upload';
@@ -17,6 +18,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
+app.use(errors());
 app.use(exceptionHandler);
 
 app.listen(3333, () => {
