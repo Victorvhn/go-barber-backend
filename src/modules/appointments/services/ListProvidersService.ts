290dfffade2693240c918d08b4c50bffedf03b62
Victor Hugo do Nascimento <victor.hugo.nascimento58@gmail.com>
2020-12-13 17:34:27 -0300

    Cache lista de providers

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'tsyringe';
 
 import IUserRepository from '@modules/users/repositories/IUserRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 import User from '@modules/users/infra/typeorm/entities/User';
 
@@ -13,12 +14,23 @@ class ListProvidersService {
   constructor(
     @inject('UserRepository')
     private userRepository: IUserRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ userId }: IRequest): Promise<User[]> {
-    const users = await this.userRepository.findAllProviders({
-      exceptUserId: userId,
-    });
+    let users = await this.cacheProvider.recover<User[]>(
+      `providers-list:${userId}`,
+    );
+
+    if (!users) {
+      users = await this.userRepository.findAllProviders({
+        exceptUserId: userId,
+      });
+
+      await this.cacheProvider.save(`providers-list:${userId}`, users);
+    }
 
     return users;
   }
