261b7d1dba7eba3502d573deb721e5f1292c2172
Victor Hugo do Nascimento <victor.hugo.nascimento58@gmail.com>
2020-12-19 13:12:55 -0300

    Correção 3

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -29,8 +29,8 @@ class ListProviderMonthAvailabilityService {
     const appointments = await this.appointmentsRepository.findAllInMonthFromProvider(
       {
         provider_id,
-        year,
         month,
+        year,
       },
     );
 
@@ -42,16 +42,13 @@ class ListProviderMonthAvailabilityService {
     );
 
     const availability = eachDayArray.map(day => {
-      const compareDate = new Date(year, month - 1, day, 23, 59, 59);
-
       const appointmentsInDay = appointments.filter(appointment => {
         return getDate(appointment.date) === day;
       });
 
       return {
         day,
-        available:
-          isAfter(compareDate, new Date()) && appointmentsInDay.length < 10,
+        available: appointmentsInDay.length < 10,
       };
     });
 
